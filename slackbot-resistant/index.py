f2aede2e1058a76e473012fc46e94fb631ed15c5
sot528 <sot528@gmail.com>
2017-08-29 15:05:20 +0900

    change messages.

--- slackbot-resistant/index.py
+++ slackbot-resistant/index.py
@@ -7,13 +7,32 @@ slack_token = os.environ["ALIS_FRAUD_PREVENTION_SLACK_API_TOKEN"]
 sc = SlackClient(slack_token)
 
 message = u'''
-Please be aware of Slackbot fraud! If you receive any messages from Slackbot, *it best to treat them as fraudulent at first* . 
-Slackbot詐欺に気を付けてください！ *Slackbotからのメッセージはすべて詐欺と考えてください。*
-请注意来自Slackbot的虚假信息！*所有来自Slackbot的信息均为虚假信息！*
+:warning: :warning: :warning: :warning: :warning: :warning: :warning: :warning: :warning: :warning: :warning: :warning:
 
-We will *NEVER* give you the ICO's ETH address via Slack! 
-また、Slack上でICO用のETHアドレスを書くことは *絶対にありません* !
-此外，绝对不要在Slack上发布ICO的任何以太坊地址！
+For the new member: *Please be aware of fraud on Slack.*
+    
+- Please be aware of Slackbot fraud! If you receive any messages from Slackbot, *it best to treat them as fraudulent at first* . 
+- We will *NEVER* give you the ICO's ETH address via Slack! 
+- Please think like that: *Every direct messages are fraud!!!!!*
+- All of *PreSale information is fraud!!!*
+
+
+新しくALISのSlackに登録された方へ: *Slackでの詐欺にお気をつけください。*
+
+- Slackbot詐欺に気を付けてください！ *Slackbotからのメッセージはすべて詐欺と考えてください。*
+- Slack上でICO用のETHアドレスを書くことは *絶対にありません* !
+- *すべてのダイレクトメッセージは詐欺* と考えてください！！！
+- *すべてのプリセールの勧誘は詐欺* です！！！
+
+
+给新会员: *请注意来自Slack的虚假信息！*
+
+- 请注意来自Slackbot的虚假信息！*所有来自Slackbot的信息均为虚假信息！*
+- 此外，绝对不要在Slack上发布ICO的任何以太坊地址！
+- 请将 *所有的私人消息视为欺诈消息* ！！！
+- 所有 *预售招标是欺诈！！！*
+
+:warning: :warning: :warning: :warning: :warning: :warning: :warning: :warning: :warning: :warning: :warning: :warning:
 '''
 
 # Get all users
